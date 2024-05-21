8b7b058d72d0db4be0d8387c338f0a9be31e29a1
Quentin Gabriele <quentin.gabriele@gmail.com>
2024-05-21 17:04:37 +0200

    add experiences and seo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,html}"],
   theme: {
-    extend: {},
+    extend: {
+      margin: {
+        container: "calc((100% - 768px) / 2)",
+      },
+    },
   },
   plugins: [],
 };
